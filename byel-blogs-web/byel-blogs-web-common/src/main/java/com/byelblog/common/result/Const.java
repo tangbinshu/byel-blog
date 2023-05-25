f5d81730ca96cb930373b9fcf07355c49b57882f
唐彬舒 <tangbinshu@126.com>
2023-05-25 10:36:11 +0800

    登录注册功能实现

---
+++ byel-blogs-web/byel-blogs-web-common/src/main/java/com/byelblog/common/result/Const.java
@@ -0,0 +1,11 @@
+package com.byelblog.common.result;
+
+public class Const {
+    // 时间 缓存时间
+    public static final int TIMEOUT = 1800;// 秒
+    public static final String ON_LOGIN = "/logout.htm";
+    public static final String LOGIN_OUT = "/toLogout";
+
+    public static final String NO_INTERCEPTOR_PATH =".*/((.css)|(.js)|(images)|(login)|(register)|(anon)).*";
+
+}
